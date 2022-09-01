a25e4d90dab087061fb54866dcbedd9b166ca3f9
The King <rishi.mishra@incture.com>
2022-09-01 17:15:38 +0530

    home fragment updated

--- app/src/main/java/com/example/demandmanagement/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/demandmanagement/fragment/HomeFragment.kt
@@ -11,43 +11,9 @@ import com.example.demandmanagement.R
 import com.example.demandmanagement.adapter.ViewPagerAdapter
 import me.relex.circleindicator.CircleIndicator3
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var notificationsList = mutableListOf<String>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-//        postToList()
-//
-//        var view_pager2 = view?.findViewById<ViewPager2>(R.id.view_pager2)
-//        Log.i("ViewPager", view_pager2.toString())
-//        view_pager2?.adapter = ViewPagerAdapter(notificationsList)
-//        view_pager2?.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-//
-//        val indicator = view?.findViewById<CircleIndicator3>(R.id.indicator)
-//        indicator?.setViewPager(view_pager2)
-
-    }
 
     private fun addToList(notification: String) {
         notificationsList.add(notification)
@@ -78,25 +44,5 @@ class HomeFragment : Fragment() {
         indicator?.setViewPager(view_pager2)
 
         return view
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
--- app/src/main/java/com/example/demandmanagement/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/demandmanagement/fragment/ProfileFragment.kt
@@ -35,6 +35,8 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
