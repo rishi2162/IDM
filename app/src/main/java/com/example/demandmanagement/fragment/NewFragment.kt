486104ba8641cb3d1efa52908ebd839f4f47ef03
INC02234 <ayush.das@incture.com>
2022-09-04 11:53:09 +0530

    fragment_new UI added

--- app/src/main/java/com/example/demandmanagement/fragment/NewFragment.kt
+++ app/src/main/java/com/example/demandmanagement/fragment/NewFragment.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.GridView
 import com.example.demandmanagement.R
 
 class NewFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +19,11 @@ class NewFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_new, container, false)
         val gridSkill = view.findViewById<GridView>(R.id.gridSkill)
         gridSkill.visibility = View.GONE
+        val btnNextPage = view.findViewById<Button>(R.id.btnNextPage)
+        btnNextPage.setOnClickListener {
+            val transition = this.fragmentManager?.beginTransaction()
+            transition?.replace(R.id.frameLayout, NewFragment2())?.commit()
+        }
         return view
     }
 
