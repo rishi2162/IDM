be9df7910c75e62ee30a2e29507c1729e025afbb
The King <rishi.mishra@incture.com>
2022-09-06 11:47:41 +0530

    Arrows added to the Demand Activity Page

--- app/src/main/java/com/example/demandmanagement/fragment/TaskDetailFragment.kt
+++ app/src/main/java/com/example/demandmanagement/fragment/TaskDetailFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.demandmanagement.R
 import com.example.demandmanagement.activity.MainActivity
 
 
 class TaskDetailFragment : Fragment() {
+
+    private var fCount = 0
+    private var rCount = 4
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +28,58 @@ class TaskDetailFragment : Fragment() {
         txtBack.setOnClickListener {
             val transition = fragmentManager?.beginTransaction()
             transition?.replace(R.id.frameLayout, TaskFragment())?.commit()
+        }
+
+        var tvText = view.findViewById<TextView>(R.id.tvFulfilledNumber)
+        var btnLeft = view.findViewById<TextView>(R.id.btnLeft)
+        var btnRight = view.findViewById<TextView>(R.id.btnRight)
+
+        tvText.text = fCount.toString()
+
+        btnLeft.setOnClickListener {
+            if (fCount > 0) {
+                fCount--
+                Toast.makeText(
+                    requireActivity(),
+                    "$fCount out of $rCount demands are fulfilled!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            tvText.text = fCount.toString()
+        }
+
+        btnRight.setOnClickListener {
+
+            when (fCount) {
+                rCount -> {
+                    Toast.makeText(
+                        requireActivity(),
+                        "All demands are fulfilled!",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                }
+                rCount - 1 -> {
+                    fCount++
+                    tvText.text = fCount.toString()
+                    Toast.makeText(
+                        requireActivity(),
+                        "All demands are fulfilled!",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                }
+                else -> {
+                    fCount++
+                    tvText.text = fCount.toString()
+                    Toast.makeText(
+                        requireActivity(),
+                        "$fCount out of $rCount demands are fulfilled!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
         }
 
         return view
